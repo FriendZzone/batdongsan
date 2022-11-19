2ed6e8da5ac6d84dae4b2aa3a5dbab0dd779c4df
hdatdoo <dat.dh@omegatheme.com>
2022-11-20 02:05:56 +0700

    Init app, create menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Header from './components/Header.js';
+import Pages from './pages/index.js';
+import './assets/style/global.scss'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const dispatch = useDispatch()
+   const [loading, setLoading] = useState(false)
+   const test = useSelector(state => state.counterSlice.test)
+   return <div className="App">
+      {loading ?
+         <div>loading</div>
+         :
+         <div>
+            <div>
+               <Header />
+            </div>
+            <div>
+               <Pages />
+            </div>
+         </div>
+      }
+   </div>
 }
-
 export default App;
